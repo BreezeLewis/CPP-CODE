3fe5030bf7fc9b1594955781adbf73652df3343b
BreezeLewis <1184615922@qq.com>
2023-03-06 09:53:13 +0800

    C

---
+++ 2023.3/test_3.6/test_3.6/1189：Pell数列.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int pell[1000001], n, k;
+//int main()
+//{
+//	pell[1] = 1, pell[2] = 2;
+//	for (int i = 3; i <= 1000000; i++)
+//		pell[i] = (2 * pell[i - 1] + pell[i - 2]) % 32767;
+//	scanf("%d", &k);
+//	while (k--)
+//	{
+//		scanf("%d", &n);
+//		printf("%d\n", pell[n]);
+//	}
+//	return 0;
+//}
---
+++ 2023.3/test_3.6/test_3.6/1190：上台阶.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//long long a[72];
+//int n;
+//int main()
+//{
+//	a[1] = 1, a[2] = 2, a[3] = 4;
+//	for (int i = 4; i <= 71; i++)
+//		a[i] = a[i - 1] + a[i - 2] + a[i - 3];
+//	while (scanf("%d", &n) && n)
+//	{
+//		printf("%lld\n", a[n]);
+//	}
+//	return 0;
+//}
