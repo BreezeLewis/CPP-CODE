565e50d54f7e3122262f39b990818fec802c4ac4
BreezeLewis <112382341+BreezeLewis@users.noreply.github.com>
2023-03-09 13:48:27 +0800

    C
    C

---
+++ 2023.3/test_3.9（递归算法）/test_3.9/1198：逆波兰表达式.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//char s[100];
+////* + 11.0 12.0 + 24.0 35.0
+////+ 2 3
+//double f()
+//{
+//	scanf("%s", s);
+//	if (s[0] == '+')
+//		return f() + f();
+//	else if (s[0] == '-')
+//		return f() - f();
+//	else if (s[0] == '*')
+//		return f() * f();
+//	else if (s[0] == '/')
+//		return f() / f();
+//	else
+//		return atof(s);
+//}
+//int main()
+//{
+//	printf("%f\n", f());
+//	return 0;
+//}
