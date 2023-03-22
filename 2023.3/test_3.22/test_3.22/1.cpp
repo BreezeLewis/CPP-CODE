69ef6540aadae3b78f2695340359996a9e138b48
BreezeLewis <1184615922@qq.com>
2023-03-22 09:39:11 +0800

    C++

---
+++ 2023.3/test_3.22/test_3.22/1.cpp
@@ -0,0 +1,42 @@
+////#include <iostream>
+////#include <string>
+////using namespace std;
+////
+////class Dog
+////{
+////public:
+////	int get_age() const
+////	{
+////		return _age;
+////	}
+////	string get_name() const
+////	{
+////		return _name;
+////	}
+////	void set_age(int age)
+////	{
+////		this->_age = age;
+////	}
+////	void set_name(const string& name)
+////	{
+////		this->_name = name;
+////	}
+////private:
+////	string _name;
+////	int _age;
+////};
+//void print_Dog(const Dog& Dog) {
+//	cout << "This is my Dog:" << endl;
+//	cout << Dog.get_name() << endl;
+//	cout << Dog.get_age() << endl;
+//}
+//
+//int main() {
+//	int age;
+//	string name;
+//	cin >> name >> age;
+//	Dog Dog;
+//	Dog.set_name(name);
+//	Dog.set_age(age);
+//	print_Dog(Dog);
+//}
