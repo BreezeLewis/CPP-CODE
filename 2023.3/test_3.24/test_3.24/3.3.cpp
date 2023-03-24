80470d83c1312b6da721034e8a84711776523ecf
BreezeLewis <1184615922@qq.com>
2023-03-24 08:59:03 +0800

    C++

---
+++ 2023.3/test_3.24/test_3.24/3.3.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <iomanip>
+//#include "rectangle.h"
+//using namespace std;
+//double Rectangle::getWidth() const
+//{
+//	return this->width;
+//}
+//double Rectangle::getHeight() const
+//{
+//	return this->height;
+//}
+//void Rectangle::setWidth(double width)
+//{
+//	this->width = width;
+//}
+//void Rectangle::setHeight(double height)
+//{
+//	this->height = height;
+//}
+//double Rectangle::getArea() const
+//{
+//	return this->height * this->width;
+//}
+//double Rectangle::getPerimeter() const
+//{
+//	return 2 * (this->width + this->height);
+//}
