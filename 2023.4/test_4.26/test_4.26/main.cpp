4538e9784d447e150bdacc0d2eec4d4fbace4823
BreezeLewis <112382341+BreezeLewis@users.noreply.github.com>
2023-04-26 10:32:50 +0800

    Add files via upload

---
+++ 2023.4/test_4.26/test_4.26/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class student
+{
+public:
+    int id;
+    int score;
+    student()
+    {
+
+    }
+    student(int a, int b)
+    {
+        id = a;
+        score = b;
+    }
+    friend double average(student* p, int count);
+};
+double average(student* p, int count)
+{
+    double sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum += p[i].score;
+    }
+    double ans = sum / count;
+    return ans;
+}
+int main()
+{
+    int id, score;
+    student* p = new student[100];
+    int count = 0;
+    while (cin >> id && id && cin >> score)
+    {
+        p[count].id = id, p[count++].score = score;
+    }
+    cout << average(p, count) << endl;
+    return 0;
+}   
