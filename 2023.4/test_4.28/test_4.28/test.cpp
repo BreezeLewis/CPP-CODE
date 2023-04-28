84e297da1c07195c9ffc92b4c8ce1d50f99d7064
BreezeLewis <112382341+BreezeLewis@users.noreply.github.com>
2023-04-28 12:22:20 +0800

    Add files via upload

---
+++ 2023.4/test_4.28/test_4.28/test.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+char* trim(char* str);
+int main()
+{
+	char s[1024] = { 0 };;
+	cin.getline(s, 1024);
+	cout << trim(s) << endl;
+	return 0;
+}
+char* trim(char* str)
+{
+	char* p;
+	p = str + 10;
+	while (*p == ' ' || *p == '\0')
+	{
+		*p = '\0';
+		p--;
+	}
+	p = str;
+	while (*p == ' ')
+		p++;
+	return p;
+}
