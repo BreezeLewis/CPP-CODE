9524bdf202adaeb8303f8a6fd81864d5984e8b97
BreezeLewis <1184615922@qq.com>
2023-06-08 06:22:03 +0800

    CPP

---
+++ 2023.6/test_6.8/test_6.8/source.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int myplus(int a, int b)
+{
+    return a + b;
+}
+double myplus(double a, double b)
+{
+    return a + b;
+}
+string myplus(string a, string b)
+{
+    return a + b;
+}
