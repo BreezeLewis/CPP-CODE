8e0b1432b8901fd45476e7e823c72aec11d15120
BreezeLewis <1184615922@qq.com>
2023-07-22 19:05:59 +0800

    cpp

---
+++ 2023.7/test_7.22(vector模拟实现)/test_7.22(vector模拟实现)/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <assert.h>
+using namespace std;
+
+#include "vector.h"
+
+
+int main()
+{
+	Lewis::test_vector9();
+	return 0;
+}
