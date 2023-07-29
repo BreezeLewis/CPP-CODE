dcb739d4c3f5ec4d29ad34861cfd33dd10135581
BreezeLewis <1184615922@qq.com>
2023-07-29 09:45:14 +0800

    cpp

---
+++ 2023.7/test_7.29/test_7.29/源.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+template<typename Type>
+Type Max(const Type& a, const Type& b)
+
+{
+
+	cout << "This is Max<Type>" << endl;
+
+	return a > b ? a : b;
+
+}
+
+template<>
+int Max<int>(const int& a, const int& b)
+
+{
+
+	cout << "This is Max<int>" << endl;
+
+	return a > b ? a : b;
+
+}
+
+template<>
+char Max<char>(const char& a, const char& b)
+
+{
+
+	cout << "This is Max<char>" << endl;
+
+	return a > b ? a : b;
+
+}
+
+int Max(const int& a, const int& b)
+
+{
+
+	cout << "This is Max" << endl;
+
+	return a > b ? a : b;
+
+}
+
+int main()
+
+{
+
+	Max(10, 20);
+
+	Max(12.34, 23.45);
+
+	Max('A', 'B');
+
+	Max<int>(20, 30);
+
+	return 0;
+
+}
