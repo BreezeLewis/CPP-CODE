7abba6e4ef9234112c6bb188126ea376d858fead
BreezeLewis <1184615922@qq.com>
2023-08-01 20:53:41 +0800

    cpp

---
+++ 2023.8/test_8.1/test_8.1/源.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+class A
+
+{
+
+public:
+
+  void f1() { cout << "A::f1()" << endl; }
+
+  virtual void f2() { cout << "A::f2()" << endl; }
+
+  virtual void f3() { cout << "A::f3()" << endl; }
+
+};
+
+
+
+class B : public A
+
+{
+
+public:
+
+	virtual void f1() { cout << "B::f1()" << endl; }
+
+	virtual void f2() { cout << "B::f2()" << endl; }
+
+	void f3() { cout << "B::f3()" << endl; }
+
+};
+int main()
+{
+
+	return 0;
+}
