28f6d1392c257abcd2cb1ce4e1c6470a1533413b
BreezeLewis <1184615922@qq.com>
2023-08-03 23:06:55 +0800

    cpp

---
+++ 2023.8/test_8.3/test_8.3/源.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+	string a = "hello world";
+
+	string b = a;
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	string c = b;
+
+	c = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	a = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	return 0;
+
+}
