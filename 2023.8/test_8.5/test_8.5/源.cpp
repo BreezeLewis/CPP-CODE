574c0f5666e2f564269950e2de2e952329b1df2d
BreezeLewis <1184615922@qq.com>
2023-08-05 22:22:06 +0800

    cpp

---
+++ 2023.8/test_8.5/test_8.5/源.cpp
@@ -0,0 +1,107 @@
+//#include<iostream>
+//
+//using namespace std;
+//class B {
+//public:
+//
+//	virtual void show() {
+//		cout << "Base" << endl;
+//	}
+//};
+//class D :public B {
+//public:
+//	void show() {
+//		cout << "Derive" << endl;
+//	}
+//};
+//
+//void fun(B& ref) {
+//	ref.show();
+//}
+//
+//
+//int main()
+//{
+//	D obj;
+//	fun(obj);
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+class B {
+public:
+
+	virtual void show() {
+		cout << "Base" << endl;
+	}
+};
+class D :public B {
+public:
+	void show() {
+		cout << "Derive" << endl;
+	}
+};
+void fun(B* ptr) {
+	ptr->show();
+}
+
+int main()
+{
+	B* p = new D();
+	fun(p);
+	return 0;
+}//#include<iostream>
+//
+//using namespace std;
+//class B {
+//public:
+//
+//	virtual void show() {
+//		cout << "Base" << endl;
+//	}
+//};
+//class D :public B {
+//public:
+//	void show() {
+//		cout << "Derive" << endl;
+//	}
+//};
+//
+//void fun(B& ref) {
+//	ref.show();
+//}
+//
+//
+//int main()
+//{
+//	D obj;
+//	fun(obj);
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+class B {
+public:
+
+	virtual void show() {
+		cout << "Base" << endl;
+	}
+};
+class D :public B {
+public:
+	void show() {
+		cout << "Derive" << endl;
+	}
+};
+void fun(B* ptr) {
+	ptr->show();
+}
+
+int main()
+{
+	B* p = new D();
+	fun(p);
+	return 0;
+}
