b379e56dfeba2dac927fd6e905ce9d477661c49e
BreezeLewis <1184615922@qq.com>
2023-09-15 08:59:06 +0800

    cpp

---
+++ 2023.9/test_9.15(C#)/test_9.15(C#)/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using MySpace;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Student student = new Student();
+            student.SayHello();
+        }
+    }
+}
+
+namespace MySpace
+{
+    class Student
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World\n");
+            Console.Read();
+        }
+    }
+}
