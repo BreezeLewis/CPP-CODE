8f4c026b3510e978bc8e4078b9d08e49d3ec9980
BreezeLewis <1184615922@qq.com>
2023-09-16 09:42:49 +0800

    cpp

---
+++ 2023.9/test_9.16(C#)/test_9.16(C#)/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = a + b;   //实现两个整数相加
+
+        /*多行注释
+          就像C语言和C++
+         */
+
+        Console.Write(c.ToString());
+        Console.Read();
+
+    }
+
+}
