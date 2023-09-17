660526a4b0978f743689e700c765d15a7d30939c
BreezeLewis <1184615922@qq.com>
2023-09-17 09:40:27 +0800

    cpp

---
+++ 2023.9/test_9.17(C#)/test_9.17(C#)/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 100;
+        byte b = 200;
+        long c = 100000000000;
+        float d = 88.8f;
+        double e = 88.8888888;
+        bool f = true;
+
+        Console.WriteLine("a是int类型，a的值是" + a);
+        Console.WriteLine("b是byte类型，b的值是" + b);
+        Console.WriteLine("c是int类型，c的值是" + c);
+        Console.WriteLine("d是int类型，d的值是" + d);
+        Console.WriteLine("e是int类型，e的值是" + e);
+        Console.WriteLine("f是int类型，f的值是" + f);
+        Console.Read();
+    }
+}
