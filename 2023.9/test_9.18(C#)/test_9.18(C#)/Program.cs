6ee0b98073bd32ec4f37e9bc443a49a3465a460d
BreezeLewis <1184615922@qq.com>
2023-09-18 15:40:41 +0800

    cpp

---
+++ 2023.9/test_9.18(C#)/test_9.18(C#)/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class MyProgram
+{
+    static void Main(string[] args)
+    {
+        //int a = 100;
+        //float b = a;
+        //Console.WriteLine("整型a的值：" + a);
+        //Console.WriteLine("浮点型b的值：" + b);
+
+        //double c = 88.888888;
+        //int d = (int)c;
+        //Console.WriteLine("双精度浮点型c的值：" + c);
+        //Console.WriteLine("整型d的值：" + d);
+
+        //int e = 100;
+        //object o = e;
+        //Console.WriteLine("装箱操作：e的值{0}，装箱为o的值{1}", e, o);
+        //e = 800;
+        //Console.WriteLine("修改e的值之后：e的值{0}，装箱为o的值{1}",e, o);
+
+        //int f = (int)o;
+        //Console.WriteLine("拆箱操作：o的值为{0}，f的值为{1}", o, f);
+        //Console.Read();
+
+        //int a = 2; int b = 5;
+        //int c = a + b;
+        //Console.WriteLine("{0}和{1}的和是{2}", a, b, c) ;
+        //Console.Read();
+
+        //const double Pi = 3.14;
+        //int r = 5;
+
+        //double area = Pi * r * r;
+        //Console.WriteLine("原面积为{0}", area);
+        //Pi = 3.1415926
+        //    r = 10;
+
+
+
+    }
+}
