cdf887f116714b63d26ac6755093ae262d000756
BreezeLewis <1184615922@qq.com>
2023-09-19 08:54:28 +0800

    C#

---
+++ 2023.9/test_9.19(C#)/test_9.19(C#)/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Lewis
+{
+    static void Main(string[] args)
+    {
+        int a = 2;
+        int c = ~a;
+        Console.WriteLine(c);
+    }
+}
