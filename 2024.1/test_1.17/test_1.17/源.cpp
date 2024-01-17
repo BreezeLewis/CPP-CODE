6234b73f4387752a1cf0955928de491be57b9aa6
BreezeLewis <1184615922@qq.com>
2024-01-17 21:16:24 +0800

    C++

---
+++ 2024.1/test_1.17/test_1.17/源.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define pi 3.1415926
+
+int main()
+{
+	/*double r;
+
+	cin >> r;
+
+	printf("%.4lf %.4lf %.4lf", 2.0 * r, 2 * pi * r, pi * r * r);*/
+
+	/*int x, y;
+
+	cin >> x >> y;*/
+
+	//a + b = x
+	//2 * a + 4 * b = y
+	//2 * a + 2 * b = 2 * x
+	//2 * b = y - 2 * x
+
+	/*int b = (y - 2 * x) / 2;
+	int a = x - (y - 2 * x) / 2;
+
+	cout << a << " " << b << endl;*/
+
+	/*int a, b, c;
+
+	cin >> a >> b >> c;
+
+	cout << a * 0.2 + b * 0.3 + c * 0.5 << endl;*/
+
+	/*string s;
+
+	cin >> s;
+
+	swap(s[0], s[2]);
+
+	cout << s << endl;*/
+
+	//int n, a, b, c;
+	//cin >> n;
+	//a = n % 10;//个位 
+	//b = n / 10 % 10;//十位 
+	//c = n / 100;//百位 
+	//cout << a * 100 + b * 10 + c;//如果a是0，那么输出的数字没有百位
+
+	/*int x, y;
+	cin >> x >> y;
+
+	double male = 87, female = 85;
+
+	printf("%.4lf\n", (male * x + female * y) / (x + y));*/
+
+	printf("%5.2f", 9.6);
+
+	return 0;
+}
