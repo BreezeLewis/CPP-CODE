7ffe2879a72163b2e1e73bf2204142db2afb01ee
BreezeLewis <1184615922@qq.com>
2024-01-18 20:22:55 +0800

    C++

---
+++ 2024.1/test_1.18/test_1.18/源.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	//double a, b, c;
+
+	//cin >> a >> b >> c;
+
+	//double p = (a + b + c) / 2;
+
+	//double s = sqrt(p * (p - a) * (p - b) * (p - c));
+
+	///*cout << s << endl;*/
+
+	//printf("%.3lf\n", s);
+
+	//int a;
+	//cin >> a;
+	////cout << (a % 2 == 0 ? "yes" : "");
+
+	//cout << ((a > 1 && a < 100) ? "yes" : "");
+
+	/*int a[3] = {};
+
+	cin >> a[0] >> a[1] >> a[2];
+
+	sort(a, a + 3, greater<int>());
+
+	for (int i = 0; i < 3; i++) 
+	{
+		cout << a[i] << " ";
+	}*/
+
+	/*int t;
+	cin >> t;
+
+	cout << ((t >= 25 && t <= 30) ? "ok!" : "no!");*/
+
+
+
+
+	return 0;
+}
