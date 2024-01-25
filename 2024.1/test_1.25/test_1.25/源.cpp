768f7f088793075c7665b9108ac648db3aad9c33
BreezeLewis <1184615922@qq.com>
2024-01-25 10:41:27 +0800

    C++

---
+++ 2024.1/test_1.25/test_1.25/源.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	/*int n;
+	cin >> n;
+	vector<bool> v(n, true);
+
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 0; j < v.size(); j++)
+		{
+			if ((j + 1) % i == 0)
+				v[j] = !v[j];
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (v[i] == true)
+			cout << i + 1 << " ";
+	}*/
+
+
+
+	return 0;
+}
