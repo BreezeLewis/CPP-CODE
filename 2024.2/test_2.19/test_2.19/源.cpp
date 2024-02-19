c39b608c7dff75a59bd481a8fffa3823a946a31f
BreezeLewis <1184615922@qq.com>
2024-02-19 22:16:22 +0800

    C++

---
+++ 2024.2/test_2.19/test_2.19/源.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+
+    unordered_map<int, int> pos;
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        for (int i = 0; i < n; i++)
+            pos[inorder[i]] = i;
+        return dfs(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    TreeNode* dfs(vector<int>& pre, vector<int>& in, int pl, int pr, int il, int ir)
+    {
+        if (pl > pr) return NULL;
+        int k = pos[pre[pl]] - il;
+        TreeNode* root = new TreeNode(pre[pl]);
+        root->left = dfs(pre, in, pl + 1, pl + k, il, il + k - 1);
+        root->right = dfs(pre, in, pl + k + 1, pr, il + k + 1, ir);
+        return root;
+    }
+};
+
+int main()
+{
+
+
+	return 0;
+}
