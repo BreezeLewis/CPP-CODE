09f1acbba8c85dc8a0ed5916d8faa7cbb8f326b7
BreezeLewis <1184615922@qq.com>
2024-04-04 09:07:31 +0800

    C++

---
+++ 2024.4/test_4.4/test_4.4/AcWing 861. 二分图的最大匹配.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int N = 510, M = 100010;
+
+int n1, n2, m;
+int h[N], e[M], ne[M], idx;
+int match[N];
+bool vis[N];
+
+void add(int a, int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+bool find(int x)
+{
+    for (int i = h[x]; i != -1; i = ne[i])
+    {
+        int j = e[i];
+        if (!vis[j])
+        {
+            vis[j] = true;
+            if (match[j] == 0 || find(match[j]))
+            {
+                match[j] = x;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    cin >> n1 >> n2 >> m;
+    memset(h, -1, sizeof h);
+
+    while (m--)
+    {
+        int a, b;
+        cin >> a >> b;
+        add(a, b);
+    }
+
+    int res = 0;
+    for (int i = 1; i <= n1; i++)
+    {
+        memset(vis, false, sizeof vis);
+        if (find(i)) res++;
+    }
+
+    cout << res;
+}
+
+
+
