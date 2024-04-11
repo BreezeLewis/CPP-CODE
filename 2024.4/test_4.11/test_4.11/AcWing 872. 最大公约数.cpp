264950bb2b084f2a2f17d0c119570d46afd0ab22
BreezeLewis <1184615922@qq.com>
2024-04-11 08:02:09 +0800

    C++

---
+++ 2024.4/test_4.11/test_4.11/AcWing 872. 最大公约数.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int gcd(int a, int b)
+{
+    return b ? gcd(b, a % b) : a;
+}
+
+int main()
+{
+    cin >> n;
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        cout << gcd(a, b) << endl;
+    }
+
+    return 0;
+}
