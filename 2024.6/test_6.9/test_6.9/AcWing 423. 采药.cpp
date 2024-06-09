7b0babb8373936d885f7b48704f9bc1a941a934d
卢昀生 <1184615922@qq.com>
2024-06-09 09:08:03 +0800

    C++

---
+++ 2024.6/test_6.9/test_6.9/AcWing 423. 采药.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1010;
+int n, m;
+int f[N];
+
+int main()
+{
+    cin >> m >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int v, w;
+        cin >> v >> w;
+        for (int j = m; j >= v; j--)
+            f[j] = max(f[j], f[j - v] + w);
+    }
+
+    cout << f[m] << endl;
+
+    return 0;
+}
