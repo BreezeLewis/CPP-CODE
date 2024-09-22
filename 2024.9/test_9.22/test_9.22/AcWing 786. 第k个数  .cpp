836e6046ef6a681aee8483b7080d79b69101797e
卢昀生 <1184615922@qq.com>
2024-09-22 15:02:06 +0800

    C++

---
+++ 2024.9/test_9.22/test_9.22/AcWing 786. 第k个数  .cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = (int)1e6 + 10;
+
+int n, k, q[N];
+
+void quick_sort(int q[], int l, int r)
+{
+    if (l >= r) return;
+
+    int x = q[l + r >> 1], i = l - 1, j = r + 1;
+
+    while (i < j)
+    {
+        while (q[++i] < x);
+        while (q[--j] > x);
+
+        if (i < j) swap(q[i], q[j]);
+    }
+
+    quick_sort(q, l, j);
+    quick_sort(q, j + 1, r);
+}
+
+int main()
+{
+    scanf("%d%d", &n, &k);
+
+    for (int i = 0; i < n; i++) scanf("%d", &q[i]);
+
+    quick_sort(q, 0, n - 1);
+
+    printf("%d\n", q[k - 1]);
+
+    return 0;
+}
