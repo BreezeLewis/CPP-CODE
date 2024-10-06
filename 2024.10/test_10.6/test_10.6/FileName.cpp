aac26aacaf7d2f7d3cf1e098a2c7dac3ecf530bb
卢昀生 <1184615922@qq.com>
2024-10-06 08:45:57 +0800

    C++

---
+++ 2024.10/test_10.6/test_10.6/FileName.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int duplicateInArray(vector<int>& nums) {
+        int n = nums.size();
+        for (auto x : nums)
+            if (x < 0 || x >= n)
+                return -1;
+        for (int i = 0; i < n; i++) {
+            while (nums[nums[i]] != nums[i]) swap(nums[i], nums[nums[i]]);
+            if (nums[i] != i) return nums[i];
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    return 0;
+}
