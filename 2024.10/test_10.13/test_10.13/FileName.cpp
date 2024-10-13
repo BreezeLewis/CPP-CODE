f94c02b6327c66fcf84bccdfb342cc2ae7ed2e8a
卢昀生 <1184615922@qq.com>
2024-10-13 09:50:02 +0800

    C++

---
+++ 2024.10/test_10.13/test_10.13/FileName.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int ans[] = { 0,2,7,5,30,169,441,1872,7632,1740,93313,459901,1358657,2504881 };
+
+	int k;
+	while (cin >> k && k)
+		cout << ans[k] << endl;
+}
