1132c741feb143a8f1a76705e9f554ded6a0732c
卢昀生 <1184615922@qq.com>
2024-11-21 09:38:43 +0800

    C++

---
+++ 2024.11/test_11.21/test_11.21/A - 排序.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+
+	sort(a.begin(), a.end());
+	for (int i = 0; i < n; ++i)
+		cout << a[i] << " ";
+	cout << endl;
+
+	return 0;
+}
