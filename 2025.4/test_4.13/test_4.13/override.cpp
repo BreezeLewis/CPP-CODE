d6720353ebc8ebe54b445fa72345d7b5df3270c0
卢昀生 <1184615922@qq.com>
2025-04-13 12:56:22 +0800

    C++

---
+++ 2025.4/test_4.13/test_4.13/override.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//class Animal {
+//public:
+//    virtual void speak() {
+//        std::cout << "Animal speaks" << std::endl;
+//    }
+//};
+//
+//class Dog : public Animal {
+//public:
+//    void speak() override {
+//        std::cout << "Dog barks" << std::endl;
+//    }
+//};
+//
+//int main()
+//{
+//    Animal* a = new Dog();
+//    a->speak(); // Êä³ö Dog barks
+//}
+
+#include <iostream>
+
+
+int main()
+{
+
+}
