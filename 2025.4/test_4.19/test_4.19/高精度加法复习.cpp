d9b9e76b14f359a109e49a6cde92e79876523738
卢昀生 <1184615922@qq.com>
2025-04-19 10:05:57 +0800

    C++

---
+++ 2025.4/test_4.19/test_4.19/高精度加法复习.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> add(vector<int>& A, vector<int>& B)
+{
+    int t = 0;
+    vector<int> ret;
+    for (int i = 0; i < A.size() || i < B.size(); i++)
+    {
+        if (i < A.size()) t += A[i];
+        if (i < B.size()) t += B[i];
+        ret.push_back(t % 10);
+        t /= 10;
+    }
+    if (t) ret.push_back(t);
+
+    return ret;
+}
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    vector<int> A, B;
+    for (int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0');
+    for (int i = b.size() - 1; i >= 0; i--) B.push_back(b[i] - '0');
+
+    auto C = add(A, B);
+    for (int i = C.size() - 1; i >= 0; i--) cout << C[i];
+
+    return 0;
+}
